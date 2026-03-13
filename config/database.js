38e61aeb874f067e5df9dea4f6bebd0d679164fa
manjeetsinghrandhawa <manjeetsinghh052003@gmail.com>
2026-03-13 15:28:04 +0530

    Updated_database.js_file

--- config/database.js
+++ config/database.js
@@ -1,32 +1,28 @@
-import { Sequelize } from "sequelize";   
-import dotenv from "dotenv";            
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import dns from "dns";
 
 dotenv.config({ path: "./config/.env" });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,  
-  process.env.DB_USER,  
-  process.env.DB_PASS,  
-  {
-    host: process.env.DB_HOST,   
-    port: process.env.DB_PORT,   
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,          
-        rejectUnauthorized: false,
-      },
-    },
-     logging: false, 
-       timezone: "+00:00",
-      pool: {
-      max: 15,        // ⬅️ VERY IMPORTANT
-      min: 0,
-      acquire: 60000, // ⬅️ prevent timeout
-      idle: 10000,
-    },
-  }
-);
+// Force IPv4 first (fixes DNS issues with Render)
+dns.setDefaultResultOrder("ipv4first");
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  timezone: "+00:00",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 15,
+    min: 0,
+    acquire: 60000,
+    idle: 10000,
+  },
+});
 
 export default sequelize;
